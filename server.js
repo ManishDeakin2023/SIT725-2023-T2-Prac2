f2ee9b2dfad17387eced196fa1bb507795b82a6e
Manish.Jadhav <s223541093@deakin.edu.au>
2023-07-18 18:13:59 +1000

    commit 2 and added api to make addition of 2 number

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.get('/', (req,res) =>
     res.render('index.html');
 });
 
-app.
+app.get('/addTwoNumbers', (req, res) => {
+    let num1 = req.query.number1;
+    let num2 = req.query.number2;
+    let result = parseInt(num1) + parseInt(num2);
+
+    let code = 200;
+    let msg = 'success';
+
+    let resObj = { data: result, statusCode: code, message: msg }
+    res.json(resObj);
+});
 
 app.listen(port, () => {
     console.log('server is started');
