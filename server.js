1fec374c16a5bcdf0f022c00e62e877c3a126cba
Manish.Jadhav <s223541093@deakin.edu.au>
2023-07-18 17:48:56 +1000

    commit 1

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/'));
+app.get('/', (req,res) =>
+{
+    res.render('index.html');
+});
+
+app.
+
+app.listen(port, () => {
+    console.log('server is started');
+});
